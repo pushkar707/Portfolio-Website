bca2cfd8a9d5b578213eb510f231b6d3d9a9bd96
Pushkar <bansalpushkar100@gmail.com>
2023-01-07 12:29:59 +0530

    small content change

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
                     Hi, I'm <span>Pushkar Bansal. </span>
                     A programmer and web developer. 
                 </h1>
-                <p>I love to code, create beautiful and functional websites and programs. <br/><br/>
-                I started programming during lockdown, started web developement, python and then I went more and more into it, learned many frameworks, libraries, technologies and created a lot of projects
+                <p>I love to code and create beautiful and functional websites and programs. <br/><br/>
+                I started programming during the covid lockdown, started web development, python and then I went more and more into it, learned many frameworks, libraries, and technologies and created a lot of projects
                 </p>
                 <div className="btn-con">
-                    <a href="https://drive.google.com/file/d/11dhNc4bKf7boKuWwAZaT7e6WfROFLRcD/view?usp=sharing" target="_blank" className="main-btn">
+                    <a href="https://drive.google.com/file/d/11dhNc4bKf7boKuWwAZaT7e6WfROFLRcD/view?usp=sharing" rel='noreferrer' target="_blank" className="main-btn">
                         <span className="btn-text">Download Resume</span>
                         <span className="btn-icon"><FontAwesomeIcon icon={faDownload}/></span>
                     </a>
